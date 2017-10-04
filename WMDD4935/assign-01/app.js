69a5b71f014beb7e22434c7141fc7f98905a98e9
goliveira-daniel <goliveira.daniel@gmail.com>
2017-10-04 15:30:54 -0700

    code cleaned, added function to get post text

--- WMDD4935/assign-01/app.js
+++ WMDD4935/assign-01/app.js
@@ -1,4 +1,3 @@
-// const http = require('http')
 const fs = require('fs')
 
 const folders = {
@@ -6,52 +5,45 @@ const folders = {
     templates: './templates',
     build: './build'
 }
-// http.createServer((req, res) => {
-//     if (req.url === '/') {
-//         res.writeHead(200, {'Content-Type': 'text/html'})
-//         fs.readFile('./static/index.html', 'utf8', (err, data) => {
-//             if (err) console.error(err)
-//             res.end(data)
-//             })
-//     }
-// }).listen(3000)
-
-function getPostsList(dir) {
-    // let arrFiles = '<ul>'
-    // fs.readdir returns an array of files in the directory
-    // arrFiles += fs.readdir(dir, (err, files) => {
-    //     if (err) console.log(err)
-    //     let innerStr = ''
-    //     // use forEach() to loop over the files
-    //     files.forEach((file) => {
-    //         innerStr += '<li>' + file + '</li>'
-    //     })
-    //         // read the files, we need to provide the directory again as the array only contains file names
-    //         // fs.readFile(`${dir}/${file}`, 'utf8', (err, data) => {
-    //             // console.log(file)
-    //         // })
-    //     return innerStr
-    // })
-    let li = ''
-    fs.readdirSync(dir).forEach(file => {li += '<li>' + file + '</li>'})
-    // arrFiles += li
-    return '<ul>' + li + '</ul>'
-}
-let dataToWrite = fs.readFileSync('./templates/index_h.html', 'utf8') + getPostsList(folders.posts) + fs.readFileSync('./templates/index_f.html', 'utf8')
-
-fs.writeFile(folders.build + '/index.html', dataToWrite.trim(), 'utf8', (err) => {
-    if (err) console.log(err)
-    console.log('File (re-)created')
-})
+getPostsText(folders.posts);
+writeFile('index.html', dataToWrite());
 
 console.log(`Watching for new posts at ${folders.posts}. Press CTRL + C to stop the watcher and finish execution.`)
+
 fs.watch(folders.posts,'utf8', (event, file) => {
     if (file) {
         console.log(`File ${file} changed. Generating new index.html...`);
-        fs.writeFile(folders.build + '/index.html', dataToWrite.trim(), 'utf8', (err) => {
-            if (err) console.log(err)
-            console.log(`File ${file} (re-)created`)
-        })
-        // Prints: <Buffer ...>
+        writeFile('index.html', dataToWrite())
     }
 })
+
+function getPostsNames(dir) {
+    let li = ''
+    fs.readdirSync(dir).forEach(file => {li += '<li>' + file + '</li>'})
+    return '<ul>' + li + '</ul>'
+}
+
+function getPostsText(dir) {
+    // fs.readdir returns an array of files in the directory
+    fs.readdir(dir, (err, files) => {
+        if (err) console.log(err)
+        // use forEach() to loop over the files
+        files.forEach((file) => {
+            // read the files, we need to provide the directory again as the array only contains file names
+            fs.readFile(`${dir}/${file}`, 'utf8', (err, data) => {
+                console.log(data)
+            })
+        })
+    })
+}
+
+function dataToWrite() {
+    return fs.readFileSync(folders.templates + '/index_h.html', 'utf8') + getPostsNames(folders.posts) + fs.readFileSync(folders.templates + '/index_f.html', 'utf8')
+}
+
+function writeFile(file, text) {
+    fs.writeFile(folders.build + '/' + file, text, 'utf8', (err) => {
+        if(err) console.log(err);
+        console.log('File (re-)created');
+    });
+}
