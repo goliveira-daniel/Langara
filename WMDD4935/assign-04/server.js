04ff1e9a9ff7a052ff75b58c56827a8245a23d6e
goliveira-daniel <goliveira.daniel@gmail.com>
2017-12-02 21:41:09 -0800

    Added authentication methods

--- WMDD4935/assign-04/server.js
+++ WMDD4935/assign-04/server.js
@@ -1,4 +1,5 @@
 const Hapi = require('hapi')
+const AuthBearer = require('hapi-auth-bearer-token')
 
 const server = new Hapi.Server()
 
@@ -7,11 +8,27 @@ server.connection({
     host: 'localhost'
 })
 
-server.route(require('./routes'))
-
-server.start((err) => {
-    if (err) {
-        throw err
+const simpleAuth = (token, callback) => {
+    // for testing purposes I am just setting the token to secret
+    // in a real API you would want to get a value from a database
+    if (token === 'secret') {
+        return callback(null, true, {token: token})
     }
-    console.log('server is listening at: ', server.info.uri)
+    return callback(null, false)
+}
+
+server.register(AuthBearer, (err) => {
+    // this creates a new strategy, api that is using a 'bearer-access-token' scheme
+    server.auth.strategy('api', 'bearer-access-token', {
+        validateFunc: simpleAuth
+    })
+
+    server.route(require('./routes'))
+
+    server.start((err) => {
+        if (err) {
+            throw err
+        }
+        console.log('server is listening at: ', server.info.uri)
+})
 })
