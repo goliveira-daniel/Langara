9975e14c0e12cf3a65cf7d337003930d2e1a53c2
goliveira-daniel <goliveira.daniel@gmail.com>
2017-12-02 18:33:45 -0800

    Added POST and DELETE methods

--- WMDD4935/assign-04/routes.js
+++ WMDD4935/assign-04/routes.js
@@ -44,6 +44,7 @@ module.exports = [
                         }
                     }, function (errorObject) {
                         console.log("The read failed: " + errorObject.code);
+                        return reply ('Server error').code(404)
                 });
             } else {
                 return reply ('query not recognized. try searching by book author, title or genre').code(404)
@@ -53,19 +54,74 @@ module.exports = [
                 return reply(data.val())
                 }, function (errorObject) {
                     console.log("The read failed: " + errorObject.code);
+                    return reply ('Server error').code(404)
             });
         }
     }
 },
 {
     method: 'GET',
-    path: '/books/{bookID*}',
+    path: '/books/{bookID}',
     handler: (request, reply) => {
         let book = firebase.database().ref('books/' + request.params.bookID)
         book.on('value', function (data) {
             return reply(data)
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+            return reply ('Server error').code(404)
         })
         // let book = books.find({_id: Number(request.params.bookID)})
+    }
+},
+{
+    method: 'DELETE',
+    path: '/books/{bookID}',
+    handler: (request, reply) => {
+        let ref = firebase.database().ref('books/') 
+        ref.on('value', function(snapshot) {
+            snapshot.forEach(child => {
+                if (child.val().id == request.params.bookID) {
+                    ref.child(child.key).remove()
+                    .then(function() {
+                        console.log("Remove succeeded.")
+                        return reply ('Book deleted')
+                    })
+                }
+                return reply ('Book not found')
+            })
+        })
+    }
+},
+{
+    method: 'POST',
+    path: '/books',
+    handler: (request, reply) => {
+        const book = request.payload
+        console.log(book)
+        firebase.database().ref('books/').push().set(book)
+        .then(function() {
+            console.log('Synchronization succeeded');
+            return reply('Synchronization succeeded');
+        })
+        .catch(function(error) {
+            console.log('Synchronization failed');
+            return reply('Synchronization failed');
+        });
+        // firebase.database().ref('books/').set({
+        //     id: book.id,
+        //     title: book.title,
+        //     author: book.author,
+        //     genre: book.genre,
+        //     publication: {
+        //         date: book.publication.date,
+        //         publisher: book.publication.publisher
+        //     },
+        //     copies: {
+        //         available: book.copies.available,
+        //         editon: book.copies.edition,
+        //         borrower: books.copies.borrower
+        //     }
+        // });
     }
 }
 ]
