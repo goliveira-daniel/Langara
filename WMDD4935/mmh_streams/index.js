46657eb95e7efa0673e04a7c5562555a667c6d38
goliveira-daniel <goliveira.daniel@gmail.com>
2017-12-03 23:57:38 -0800

    Added count to 6 exercises

---
+++ WMDD4935/mmh_streams/index.js
@@ -0,0 +1,56 @@
+const Hapi = require('hapi')
+// const Inert = require('inert');
+const path = require('path');
+const fs = require('fs');
+// const vision = require('vision');
+const rot13 = require('rot13-transform');
+
+const server = new Hapi.Server()
+
+server.connection({
+    port: Number(process.argv[2] || 8080),
+    host: 'localhost'
+})
+
+// var options = {
+//     views: {
+//         path: path.join(__dirname, '/templates'),
+//         engines: {
+//             html: require('handlebars')
+//         }
+//     }
+// };
+
+// server.register(vision, function (err) {
+//     if (err) throw err;
+// });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+        handler: (req, res) => { 
+            let file = fs.createReadStream(path.join(__dirname, 'file.txt'));
+            res(file.pipe(through(function (buf) {
+                this.queue(rot13(buf.toString()));
+              }, function () {
+                this.queue(null);
+              })));
+        }
+    }    
+})
+
+// server.views({
+//     engines: {
+//         html: require('handlebars')
+//     },
+//     helpersPath: path.join(__dirname, 'helpers'),
+//     path: path.join(__dirname, 'template')
+// });
+
+server.start((err) => {
+    if (err) {
+        throw err
+    }
+    console.log('server is listening at: ', server.info.uri)
+})
