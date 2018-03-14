0a14d5be24da20a8f4a3255d6952e1fe5fcf58de
Daniel Garcia de Oliveira <goliveira.daniel@gmail.com>
2018-03-14 01:40:01 -0700

    Added function to check winner

--- WMDD4940/lab5/app/src/main/java/com/wmdd4940/langara/lab5/MainActivity.java
+++ WMDD4940/lab5/app/src/main/java/com/wmdd4940/langara/lab5/MainActivity.java
@@ -12,18 +12,24 @@ import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn0, btn1, btn2, btn3, btn4,btn5, btn6, btn7, btn8, gameControls;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private Button btn6;
+    private Button btn7;
+    private Button btn8;
+    private Button gameControls;
 //    winningGames =        [[0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-    private List<List<Integer>> winningGames = Arrays.asList([3,4,5]);
-//    winningGames = Arrays.asList ()
-    //    = new int[][] {[0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]};
+    private List<List<Integer>> winningGames = Arrays.asList(Arrays.asList(3,4,5), Arrays.asList(0,1,2),Arrays.asList(6,7,8),Arrays.asList(0,3,6),Arrays.asList(1,4,7),Arrays.asList(2,5,8),Arrays.asList(0,4,8),Arrays.asList(2,4,6));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Player player = new Player();
-        btn0 = findViewById(R.id.btn0);
+        Button btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
@@ -48,9 +54,18 @@ public class MainActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     button.setText(player.getCurrPlayer());
-                    player.invertPlayer();
-                    button.setEnabled(false);
-                    tv.setText(getString(R.string.playerTurn,player.getCurrPlayer()));
+                    if (player.getCurrPlayer() == "X") {
+                        player.playerXTiles.add(Integer.parseInt(v.getTag().toString()));
+                    } else {
+                        player.playerOTiles.add(Integer.parseInt(v.getTag().toString()));
+                    }
+                    if (player.checkWinner()) {
+                        tv.setText(getString(R.string.playerWin,player.getCurrPlayer()));
+                    } else {
+                        player.invertPlayer();
+                        button.setEnabled(false);
+                        tv.setText(getString(R.string.playerTurn,player.getCurrPlayer()));
+                    }
                 }
             });
         }
@@ -60,6 +75,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 for (final Button button:buttons) {
                     button.setEnabled(true);
+                    player.playerOTiles.clear();
+                    player.playerXTiles.clear();
+                    player.randomPlayer();
+                    button.setText("");
                     tv.setText(getString(R.string.playerTurn,player.getCurrPlayer()));
                 }
             }
@@ -68,13 +87,28 @@ public class MainActivity extends AppCompatActivity {
 
     class Player {
         private String currPlayer;
+        List<Integer> playerXTiles = new ArrayList<>();
+        List<Integer> playerOTiles = new ArrayList<>();
 
-        public Player() {
+        Player() {
             setCurrPlayer(randomPlayer());
         }
 
-        public String getCurrPlayer() {
+        String getCurrPlayer() {
             return currPlayer;
+        }
+
+        boolean checkWinner () {
+            if (currPlayer.equals("X")) {
+                for (List list:winningGames) {
+                    if (playerXTiles.contains(list)) {return true;}
+                }
+            } else {
+                for (List list:winningGames) {
+                    if (playerOTiles.contains(list)) {return true;}
+                }
+            }
+            return false;
         }
 
         private void setCurrPlayer(String currPlayer) {
