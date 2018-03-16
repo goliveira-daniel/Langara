aef634daf6f23667814629a253e6a95d6d0ce86f
Daniel Garcia de Oliveira <goliveira.daniel@gmail.com>
2018-03-15 22:58:18 -0700

    Hackathon project initial commit

---
+++ WMDD4950/Hackathon project/processImage.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const exec = require('child_process').exec;
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const storage = require('@google-cloud/storage')();
+
+/**
+ * Triggered from a message on a Cloud Storage bucket.
+ *
+ * @param {!Object} event The Cloud Functions event.
+ * @param {!Function} The callback function.
+ */
+exports.processImage = (event, callback) => {
+  console.log('Processing file: ' + event.data.name);
+  const object = event.data;
+
+  // Exit if this is a deletion or a deploy event.
+  if (object.resourceState === 'not_exists') {
+    console.log('This is a deletion event.');
+    return;
+  } else if (!object.name) {
+    console.log('This is a deploy event.');
+    return;
+  }
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!object.contentType.startsWith('image/')) {
+    console.log('This is not an image.');
+    return null;
+  }
+  
+  const file = storage.bucket(object.bucket).file(object.name);
+
+  console.log(`Analyzing ${file.name}.`);
+  return sliceImage(file);
+//  callback();
+};
+
+// Slice the given file using ImageMagick.
+function sliceImage (file) {
+  const tempLocalFilename = `/tmp/${path.parse(file.name).base}`;
+
+  // Download file from bucket.
+  return file.download({ destination: tempLocalFilename })
+    .catch((err) => {
+      console.error('Failed to download file.', err);
+      return Promise.reject(err);
+    })
+    .then(() => {
+      console.log(`Image ${file.name} has been downloaded to ${tempLocalFilename}.`);
+
+      // Blur the image using ImageMagick.
+      return new Promise((resolve, reject) => {
+        exec(`convert ${tempLocalFilename} -crop 4x4@ +repage  +adjoin ${file.name}@_%d.jpg`, { stdio: 'ignore' }, (err, stdout) => {
+          if (err) {
+            console.error('Failed to slice image.', err);
+            reject(err);
+          } else {
+            console.log(stdout);
+            resolve(stdout);
+          }
+        });
+      });
+    })
+    .then(() => {
+      console.log(`Image ${file.name} has been sliced.`);
+
+      // Upload the Sliced image back into the bucket.
+      return file.bucket.upload(tempLocalFilename, { destination: file.name })
+        .catch((err) => {
+          console.error('Failed to upload slice image.', err);
+          return Promise.reject(err);
+        });
+    })
+    .then(() => {
+      console.log(`Image ${file.name} has been sliced.`);
+
+      // Upload the Sliced image back into the bucket.
+      return file.bucket.upload(tempLocalFilename + '_1.jpg', { destination: 'test.jpg' })
+        .catch((err) => {
+          console.error('Failed to upload slice image.', err);
+          return Promise.reject(err);
+        });
+    })
+    .then(() => {
+      console.log(`Sliced image has been uploaded to ${'test.jpg'}.`);
+
+      // Delete the temporary file.
+      return new Promise((resolve, reject) => {
+        fs.unlink(tempLocalFilename, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    });
+}
