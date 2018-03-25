141c4cc63095ae3f7ebe4b4966c6a5f842e5134c
Daniel Garcia de Oliveira <goliveira.daniel@gmail.com>
2018-03-25 01:27:50 -0700

    Lab 6 - Initial commit

--- WMDD4940/lab6/app/src/main/java/com/wmdd4940/langara/lab6/MainActivity.java
+++ WMDD4940/lab6/app/src/main/java/com/wmdd4940/langara/lab6/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.GridLayout;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 //import android.widget.RelativeLayout;
@@ -19,17 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<View> ivList = new ArrayList();
-        ArrayList<String> imgList = new ArrayList();
+        List<String> imgList = Arrays.asList("img1.png","img2.png","img3.png","img4.png","img5.png","img6.png","img7.png","img8.png","img9.png","img10.png","img11.png","img12.png","img13.png","img14.png","img15.png","blank.png");
         GridLayout grid = findViewById(R.id.grid);
         for (int i = 0; i<grid.getChildCount(); i++) {
             final View v = grid.getChildAt(i);
             if (v instanceof ImageView) {
-                Log.d(TAG,"Found imageview: " + v.getId());
+                Log.d(TAG, String.format("Found imageview: %d", v.getId()));
                 ivList.add(v);
 //                imgList.add(((ImageView) v).getDrawable());
-
             }
         }
+        for (int i = 0; i<ivList.listIterator())
         Collections.shuffle(imgList);
     }
 }
