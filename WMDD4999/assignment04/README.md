cb80b04ec51d87415a6b68d9f0378ae968987d68
Daniel Garcia de Oliveira <goliveira.daniel@gmail.com>
2018-03-27 20:49:55 -0700

    Assignment 4 - Initial commit

---
+++ WMDD4999/assignment04/README.md
@@ -0,0 +1,7 @@
+# Instructions to test this project
+1. Clone this repo
+2. Create a file named '.env'
+3. Create 4 entries for your Twilio ID, Token, From and To phone numbers
+4. Run command 'npm install' to install dependencies
+5. Deploy function using SAM local or zipping up all files and importing them into AWS website
+6. Test function using file test-event.json as test event
---
+++ WMDD4999/assignment04/index.js
@@ -0,0 +1,29 @@
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
+const TWILIO_PHONE_FROM = process.env.TWILIO_PHONE_FROM
+const TWILIO_PHONE_TO = process.env.TWILIO_PHONE_TO
+const request = require('request-promise-native')
+
+exports.handler = (event, context, callback) => {
+    request.post({
+        url: `https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Messages.json`,
+        json: true,
+        'auth': {
+            'user': TWILIO_ACCOUNT_SID,
+            'pass': TWILIO_AUTH_TOKEN
+        },
+        form: {
+            From: TWILIO_PHONE_FROM,
+            To: TWILIO_PHONE_TO,
+            Body: `the new file ${event.Records[0].s3.object.key} was added to your S3 bucket`
+        }
+    })
+    .then((data) => {
+        console.log(`Message successfully sent through SMS to ${TWILIO_PHONE_TO}`)
+        return callback(null, true)
+    })
+    .catch((err) => {
+        console.error(err)
+        return callback(err)
+    })
+}; 
