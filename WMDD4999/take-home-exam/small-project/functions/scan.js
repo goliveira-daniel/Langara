5d7abd8a38d8af51d05a1df4af5d910570e9d1e8
Daniel Garcia de Oliveira <goliveira.daniel@gmail.com>
2018-04-06 00:14:55 -0700

    Refactored function

--- WMDD4999/take-home-exam/small-project/functions/scan.js
+++ WMDD4999/take-home-exam/small-project/functions/scan.js
@@ -1,29 +1,42 @@
-'use strict';
+"use strict";
 
-const doc = require('dynamodb-doc');
-const dynamo = new doc.DynamoDB();
+const uuid = require("uuid");
+// const doc = require("dynamodb-doc");
+// const dynamo = new doc.DynamoDB();
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.scan = (event, context, callback) => {
-	event.Records.forEach((record) => {
-		console.log(`New audio file detected: ${record.s3.object.key} (${record.s3.object.size} bytes)`);
-		return dynamo.putItem({
-			'TableName': 'wmdd4999-assignment03',
-			'Item' : {
-				'FileName': {'S': record.s3.object.key},
-				'EventTime': {'S': record.eventTime},
-				'id': record.s3.object.sequencer
-			}
-		}, (error) => {
-			// handle potential errors
-			if (error) {
-				console.error(error);
-				callback(null, {
-					statusCode: error.statusCode || 501,
-					headers: { 'Content-Type': 'text/plain' },
-					body: 'Couldn\'t create the todo item.',
-				});
-				return;
-			}
-		});
-	});
+  const timestamp = new Date().getTime();
+  let params = {};
+
+  event.Records.forEach(record => {
+    params = {
+      TableName: process.env.AUDIO_DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1(),
+        FileName: record.s3.object.key,
+        createdAt: timestamp,
+        updatedAt: timestamp
+      }
+    };
+
+    dynamoDb.put(params, error => {
+      if (error) {
+        console.error(error);
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { "Content-Type": "text/plain" },
+          body: "Couldn't create the playlist item."
+        });
+        return;
+      }
+      // create a response
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(params.Item)
+      };
+      callback(null, response);
+    });
+  });
 };
