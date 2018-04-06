ca666456d4477c0c32d9eb2e9601b62ae7a5f646
Daniel Garcia de Oliveira <goliveira.daniel@gmail.com>
2018-04-06 04:03:59 -0700

    Scans the table with the string parameter

--- WMDD4999/take-home-exam/small-project/functions/get.js
+++ WMDD4999/take-home-exam/small-project/functions/get.js
@@ -6,20 +6,22 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.get = (event, context, callback) => {
   const params = {
     TableName: process.env.AUDIO_DYNAMODB_TABLE,
-    Key: {
-      id: event.pathParameters.id,
-    },
+    // KeyConditionExpression: 'contains(FileName, :queryString)', 
+    FilterExpression: 'contains(FileName, :queryString)', 
+    ExpressionAttributeValues: {
+      ':queryString': event.pathParameters.id
+     },
   };
 
-  // fetch todo from the database
-  dynamoDb.get(params, (error, result) => {
+  // scan audio from the database
+  dynamoDb.scan(params, (error, result) => {
     // handle potential errors
     if (error) {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todo item.',
+        body: 'Couldn\'t find the audio.',
       });
       return;
     }
@@ -27,7 +29,7 @@ module.exports.get = (event, context, callback) => {
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Item),
+      body: JSON.stringify(result),
     };
     callback(null, response);
   });
