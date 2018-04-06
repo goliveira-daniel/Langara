db9aa046133144308a2298219526e2d2e26f5134
Daniel Garcia de Oliveira <goliveira.daniel@gmail.com>
2018-04-06 04:03:31 -0700

    Check if songs exists before insert on playlist table

--- WMDD4999/take-home-exam/small-project/functions/create.js
+++ WMDD4999/take-home-exam/small-project/functions/create.js
@@ -8,22 +8,46 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
-  if (typeof data.text !== 'string') {
-    console.error('Validation Failed');
-    callback(null, {
-      statusCode: 400,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Couldn\'t create the playlist item.',
-    });
-    return;
-  }
+  // if (typeof data.text !== 'string') {
+  //   console.error('Validation Failed');
+  //   callback(null, {
+  //     statusCode: 400,
+  //     headers: { 'Content-Type': 'text/plain' },
+  //     body: 'Couldn\'t create the playlist item.',
+  //   });
+  //   return;
+  // }
+  console.log(data)
+  data.songs.forEach(element => {
+    dynamoDb.query({
+      TableName: process.env.AUDIO_DYNAMODB_TABLE,
+      // KeyConditionExpression: 'contains(FileName, :queryString)', 
+      KeyConditionExpression: 'id = :queryString)',
+      ExpressionAttributeValues: {
+        ':queryString': element
+       },
+      }, (error, result) => {
+        if (error || result.Count !== 1) {
+          console.error(error);
+          callback(null, {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t find song.',
+          });
+          return;
+        }
+    })
+  });
+  
+  
+
 
   const params = {
     TableName: process.env.PLAYLIST_DYNAMODB_TABLE,
     Item: {
       id: uuid.v1(),
-      text: data.text,
-      checked: false,
+      name: data.name,
+      songs: data.songs,
       createdAt: timestamp,
       updatedAt: timestamp,
     },
@@ -37,7 +61,7 @@ module.exports.create = (event, context, callback) => {
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the playlist item.',
+        body: 'Couldn\'t create the playlist.',
       });
       return;
     }
